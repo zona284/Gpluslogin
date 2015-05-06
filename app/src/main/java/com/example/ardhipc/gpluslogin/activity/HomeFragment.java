4ab270b66047fb944d45b23788ce9b604278eed8
ardhimaarik <ardhimaarik@yahoo.co.id>
2015-05-06 12:20:12 +0700

    page in material design

---
+++ app/src/main/java/com/example/ardhipc/gpluslogin/activity/HomeFragment.java
@@ -0,0 +1,45 @@
+package com.example.ardhipc.gpluslogin.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.ardhipc.gpluslogin.R;
+
+/**
+ * Created by Ardhipc on 5/6/2015.
+ */
+public class HomeFragment extends Fragment{
+    public HomeFragment(){
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+
+        // Inflate the layout for this fragment
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
--- app/src/main/java/com/example/ardhipc/gpluslogin/activity/MainMenu.java
+++ app/src/main/java/com/example/ardhipc/gpluslogin/activity/MainMenu.java
@@ -2,12 +2,16 @@ package com.example.ardhipc.gpluslogin.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.ardhipc.gpluslogin.R;
 import com.example.ardhipc.gpluslogin.adapter.FragmentDrawer;
@@ -16,8 +20,11 @@ import com.example.ardhipc.gpluslogin.adapter.FragmentDrawer;
  * Created by Ardhipc on 5/5/2015.
  */
 public class MainMenu extends ActionBarActivity implements FragmentDrawer.FragmentDrawerListener{
+    private static String TAG = MainMenu.class.getSimpleName();
+
     private Toolbar mToolbar;
     private FragmentDrawer drawerFragment;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +39,8 @@ public class MainMenu extends ActionBarActivity implements FragmentDrawer.Fragme
                 getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
         drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout),mToolbar);
         drawerFragment.setDrawerListener(this);
+        // display the first navigation drawer view on app launch
+        displayView(0);
     }
 
     @Override
@@ -39,6 +48,42 @@ public class MainMenu extends ActionBarActivity implements FragmentDrawer.Fragme
     // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    public void onDrawerItemSelected(View view, int position) {
+        displayView(position);
+    }
+
+    private void displayView(int position) {
+        Fragment fragment = null;
+        String title = getString(R.string.app_name);
+        switch (position) {
+            case 0:
+                fragment = new HomeFragment();
+                title = getString(R.string.title_home);
+                break;
+            case 1:
+                fragment = new FriendsFragment();
+                title = getString(R.string.title_friends);
+                break;
+            case 2:
+                fragment = new MessagesFragment();
+                title = getString(R.string.title_messages);
+                break;
+            default:
+                break;
+        }
+        if (fragment != null){
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.container_body, fragment);
+            fragmentTransaction.commit();
+
+            // set the toolbar title
+            getSupportActionBar().setTitle(title);
+        }
+
     }
 
     @Override
@@ -53,11 +98,12 @@ public class MainMenu extends ActionBarActivity implements FragmentDrawer.Fragme
             return true;
         }
 
+        if(id == R.id.action_search){
+            Toast.makeText(getApplicationContext(), "Search action is selected!", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public void onDrawerItemSelected(View view, int position) {
-
-    }
 }
